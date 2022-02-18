75d54623a5189045b43b0fdae2bd59a8ded919a3
V11Playko <heinnervega20@gmail.com>
2022-02-18 07:26:59 -0500

    Introducción a servicios: crea tu primer servicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CategoriesController, ProductsController],
-  providers: [AppService],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
