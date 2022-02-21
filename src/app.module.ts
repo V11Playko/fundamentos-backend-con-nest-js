d1ebc266f4fcd9a267c672c73a5b9b3c93437d04
V11Playko <heinnervega20@gmail.com>
2022-02-21 08:19:14 -0500

    Solucion de bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/controllers/products.controller';
-import { CategoriesController } from './products/controllers/categories.controller';
-import { ProductsService } from './products/services/products.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
