26f842aacb404d1ce57d80abad7e8d6aced3192e
V11Playko <heinnervega20@gmail.com>
2022-02-23 18:44:37 -0500

    TypeORM: active record vs. repositories

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -53,20 +53,16 @@ export class ProductsController {
 
   @Post()
   create(@Body() payload: CreateProductDto) {
-    // return {
-    //   message: 'accion de crear',
-    //   payload,
-    // };
     return this.productsService.create(payload);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-    return this.productsService.update(+id, payload);
+  update(@Param('id') id: number, @Body() payload: UpdateProductDto) {
+    return this.productsService.update(id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  delete(@Param('id') id: number) {
+    return this.productsService.remove(id);
   }
 }
