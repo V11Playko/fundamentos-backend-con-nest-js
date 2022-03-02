48ef42cad18ccd2a51f2c70a2731150210afe6e7
V11Playko <heinnervega20@gmail.com>
2022-03-02 07:22:29 -0500

    Agregando paginación

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
@@ -19,8 +22,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
